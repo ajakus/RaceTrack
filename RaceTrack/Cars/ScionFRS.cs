e4fdaa6b403ddf4af47493d6bc21a1430a44c7a0
Adam Jakus <ajakus@my.wctc.edu>
2022-03-07 18:43:08 -0600

    Add project files.

---
+++ RaceTrack/Cars/ScionFRS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class ScionFRS : RaceCar
+    { 
+        public ScionFRS()
+        {
+            Name = "Scion FR-S";
+            TopSpeed = 200;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} roars to life!");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} ends in position {Position}!");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/Me.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Me : Driver
+    {
+        public Me(ScionFRS car) : base(car)
+        {
+            Name = "Adam J";
+            SkillLevel = 15;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+
+    }
+}
